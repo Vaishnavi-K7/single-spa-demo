4a1e82f834496aa654e637d91bb8aa809880d835
Vaishnavi Khapekar <ext.vaishnavibhaskar.khapekar@transporeon.com>
2024-11-05 11:44:27 +0530

    working setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "iamcyberster";
+  const orgName = "iamvaishnavi";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
